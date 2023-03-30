c32a2cccde574f074a6ea525b8509d5d611dc074
Ömer Osman <113211889+omerosmann@users.noreply.github.com>
2023-03-30 14:17:36 +0300

    Add files via upload

---
+++ src/main/java/kodlama/io/rentacar/business/dto/responses/update/UpdateModelResponse.java
@@ -0,0 +1,17 @@
+package kodlama.io.rentacar.business.dto.responses.update;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateModelResponse {
+    private int id;
+    private String name;
+    private int brandId;
+}
