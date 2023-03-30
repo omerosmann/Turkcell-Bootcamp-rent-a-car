aba7d4e37230d17d66bbdffd35b3e0dc20167693
Ömer Osman <113211889+omerosmann@users.noreply.github.com>
2023-03-30 13:28:26 +0300

    Update GetAllModelResponse.java

--- src/main/java/kodlama/io/rentacar/business/dto/responses/get/GetAllModelResponse.java
+++ src/main/java/kodlama/io/rentacar/business/dto/responses/get/GetAllModelResponse.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class GetAllModelResponse {
+public class GetAllModelsResponse {
     private int id;
     private String name;
     private String brandName;
